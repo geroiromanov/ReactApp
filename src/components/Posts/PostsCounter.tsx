8fdb1f8561c7ca2b2811242b08975719bdde46c3
Roman Hrytsyk <r.hrytsyk@polytech.software>
2024-09-10 14:11:33 -0400

    RA-1: implemented reducer with context provider

--- src/components/Posts/PostsCounter.tsx
+++ src/components/Posts/PostsCounter.tsx
@@ -4,12 +4,14 @@ import { PostsContext } from "./PostsProvider"
 export default function PostsCounter()
 {
     const posts = useContext(PostsContext)
+    const postsTotal = posts ? posts.length : 0;
     const selectedItemsNumber = posts 
         ? posts.reduce((counter, post) => post.selected === true ? counter += 1 : counter, 0)
         : 0;
 
     return (
         <>
+            Total items: {postsTotal}
             Selected Items: {selectedItemsNumber}    
         </>
     )
--- src/components/Posts/PostsEl.tsx
+++ src/components/Posts/PostsEl.tsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { PostsContextDispatch } from "./PostsProvider";
 
-export default function PostEl({post, onChange, onSave, onDelete}) {
+export default function PostEl({post}) {
     const [isEdit, setIsEdit] = useState(false);
     const [title, setTitle] = useState(post.title);
 
+    const dispatch = useContext(PostsContextDispatch)
+
     function onEdit() {
         setIsEdit(!isEdit)
+    }
+
+    function onChange(e) {
+        dispatch({id: post.id, selected: e.target.checked, type: 'update'});
+        
+    }
+    
+    function onSave() {
+        dispatch({id: post.id, title: post.title, type: 'save'});
+        setIsEdit(false);
+    }
+
+    function onDelete() {
+        dispatch({id: post.id, type: 'delete'});
     }
 
     const tmplInput = isEdit
@@ -13,15 +30,15 @@ export default function PostEl({post, onChange, onSave, onDelete}) {
         : <label>{title}</label>
 
     const tmplBtn = isEdit 
-        ? <button onClick={() => {onSave(post.id, title); setIsEdit(false)}}>Save</button>
+        ? <button onClick={onSave}>Save</button>
         : <button onClick={onEdit}>Edit</button>
 
     return (
         <li>
-            <input type="checkbox" checked={post.selected} onChange={() => onChange(post.id)}/>
+            <input type="checkbox" checked={post.selected} onChange={onChange}/>
             {tmplInput}
             {tmplBtn}
-            <button onClick={() => onDelete(post.id)}>Delete</button>
+            <button onClick={onDelete}>Delete</button>
         
         </li>
     )
